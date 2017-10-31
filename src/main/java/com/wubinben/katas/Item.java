4f5908b1022ac2e282e24815cda5f7398d5d0bbf
Ben Wu <wubin28@gmail.com>
2017-10-31 23:09:32 +0800

    eliminate magic numbers

--- src/main/java/com/wubinben/katas/Item.java
+++ src/main/java/com/wubinben/katas/Item.java
@@ -1,12 +1,16 @@
 package com.wubinben.katas;
 
 public class Item {
+    public static final String SULFURAS = "Sulfuras";
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String BACKSTAGE_PASSES = "Backstage passes";
+    public static final String NORMAL = "Normal";
     private final int sellIn;
     private final int quality;
     private final String name;
 
     public static Item newInstanceWithNameSellInAndQuality(String name, int sellIn, int quality) {
-        if (!name.equals("Sulfuras") && (quality > 50 || quality < 0)) {
+        if (!name.equals(SULFURAS) && (quality > 50 || quality < 0)) {
             throw new IllegalArgumentException("The quality of the normal item should be between 0 and 50.");
         }
         return new Item(name, sellIn, quality);
@@ -23,13 +27,13 @@ public class Item {
     }
 
     public Item updateSellInAndQuality() {
-        if (this.name.equals("Sulfuras")) {
+        if (this.name.equals(SULFURAS)) {
             return new Item(this.name, sellIn - 1, quality);
         }
-        if (this.name.equals("Aged Brie")) {
+        if (this.name.equals(AGED_BRIE)) {
             return new Item(this.name, sellIn - 1, notGreaterThanFifty(quality + 1));
         }
-        if (this.name.equals("Backstage passes")) {
+        if (this.name.equals(BACKSTAGE_PASSES)) {
             return new Item(this.name, sellIn - 1, notGreaterThanFifty(sellIn > 10 ? quality + 1 : (sellIn > 5 ? quality + 2 : (sellIn > 0 ? quality + 3 : 0))));
         }
         return new Item(this.name, sellIn - 1, notLessThanZero((sellIn <= 0) ? quality - 2 : quality - 1));
--- src/test/java/com/wubinben/katas/GildedRoseTest.java
+++ src/test/java/com/wubinben/katas/GildedRoseTest.java
@@ -8,7 +8,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GildedRoseTest {
     @Test
     public void at_the_end_of_each_day_our_system_lowers_sellIn_and_quality_values_by_1_if_sellIn_is_greater_than_0_for_normal_items() {
-        Item item = Item.newInstanceWithNameSellInAndQuality("Normal", 1, 20);
+        Item item = Item.newInstanceWithNameSellInAndQuality(Item.NORMAL, 1, 20);
 
         Item updatedItem = item.updateSellInAndQuality();
 
@@ -18,7 +18,7 @@ public class GildedRoseTest {
 
     @Test
     public void once_the_sell_by_date_has_passed_quality_of_normal_items_degrades_twice_as_fast() {
-        Item item = Item.newInstanceWithNameSellInAndQuality("Normal", 0, 20);
+        Item item = Item.newInstanceWithNameSellInAndQuality(Item.NORMAL, 0, 20);
 
         Item updatedItem = item.updateSellInAndQuality();
 
@@ -28,7 +28,7 @@ public class GildedRoseTest {
 
     @Test
     public void the_quality_of_normal_items_should_be_greater_than_or_equal_to_0() {
-        Item item = Item.newInstanceWithNameSellInAndQuality("Normal", 0, 0);
+        Item item = Item.newInstanceWithNameSellInAndQuality(Item.NORMAL, 0, 0);
 
         Item updatedItem = item.updateSellInAndQuality();
 
@@ -38,12 +38,12 @@ public class GildedRoseTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void the_quality_of_normal_items_should_be_no_more_than_50() {
-        Item item = Item.newInstanceWithNameSellInAndQuality("Normal", 0, 51);
+        Item item = Item.newInstanceWithNameSellInAndQuality(Item.NORMAL, 0, 51);
     }
 
     @Test
     public void the_Aged_Brie_actually_increases_in_Quality_by_1_the_older_it_gets() {
-        Item item = Item.newInstanceWithNameSellInAndQuality("Aged Brie", 0, 0);
+        Item item = Item.newInstanceWithNameSellInAndQuality(Item.AGED_BRIE, 0, 0);
 
         Item updatedItem = item.updateSellInAndQuality();
 
@@ -53,7 +53,7 @@ public class GildedRoseTest {
 
     @Test
     public void the_quality_of_Aged_Brie_should_not_exceed_50() {
-        Item item = Item.newInstanceWithNameSellInAndQuality("Aged Brie", -1, 50);
+        Item item = Item.newInstanceWithNameSellInAndQuality(Item.AGED_BRIE, -1, 50);
 
         Item updatedItem = item.updateSellInAndQuality();
 
@@ -63,7 +63,7 @@ public class GildedRoseTest {
 
     @Test
     public void the_Sulfuras_is_a_legendary_item_and_as_such_its_Quality_is_80_and_it_never_alters() {
-        Item item = Item.newInstanceWithNameSellInAndQuality("Sulfuras", -1, 80);
+        Item item = Item.newInstanceWithNameSellInAndQuality(Item.SULFURAS, -1, 80);
 
         Item updatedItem = item.updateSellInAndQuality();
 
@@ -73,7 +73,7 @@ public class GildedRoseTest {
 
     @Test
     public void the_quality_of_backstage_passes_increases_by_1_when_the_sellIn_are_more_than_10_days() {
-        Item item = Item.newInstanceWithNameSellInAndQuality("Backstage passes", 11, 20);
+        Item item = Item.newInstanceWithNameSellInAndQuality(Item.BACKSTAGE_PASSES, 11, 20);
 
         Item updatedItem = item.updateSellInAndQuality();
 
@@ -83,7 +83,7 @@ public class GildedRoseTest {
 
     @Test
     public void the_quality_of_backstage_passes_increases_by_2_when_the_sellIn_are_between_5_and_10_days() {
-        Item item = Item.newInstanceWithNameSellInAndQuality("Backstage passes", 10, 20);
+        Item item = Item.newInstanceWithNameSellInAndQuality(Item.BACKSTAGE_PASSES, 10, 20);
 
         Item updatedItem = item.updateSellInAndQuality();
 
@@ -93,7 +93,7 @@ public class GildedRoseTest {
 
     @Test
     public void the_quality_of_backstage_passes_increases_by_3_when_the_sellIn_are_between_0_and_5_days() {
-        Item item = Item.newInstanceWithNameSellInAndQuality("Backstage passes", 5, 20);
+        Item item = Item.newInstanceWithNameSellInAndQuality(Item.BACKSTAGE_PASSES, 5, 20);
 
         Item updatedItem = item.updateSellInAndQuality();
 
@@ -103,7 +103,7 @@ public class GildedRoseTest {
 
     @Test
     public void the_quality_of_backstage_passes_drops_to_0_after_the_concert() {
-        Item item = Item.newInstanceWithNameSellInAndQuality("Backstage passes", 0, 20);
+        Item item = Item.newInstanceWithNameSellInAndQuality(Item.BACKSTAGE_PASSES, 0, 20);
 
         Item updatedItem = item.updateSellInAndQuality();
 
@@ -113,7 +113,7 @@ public class GildedRoseTest {
 
     @Test
     public void the_quality_of_backstage_passes_is_not_greater_than_50() {
-        Item item = Item.newInstanceWithNameSellInAndQuality("Backstage passes", 1, 50);
+        Item item = Item.newInstanceWithNameSellInAndQuality(Item.BACKSTAGE_PASSES, 1, 50);
 
         Item updatedItem = item.updateSellInAndQuality();
 
